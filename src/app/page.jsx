dd905c756977a9cd96b168cf8af3fb34fb5d0f07
muhammadaliyev <muhammadaliyev>
2025-12-17 19:47:40 +0500

    tayyor

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Cta from "@/components/Cta";
+import Cta from "@/components/main";
 import Footer from "@/components/Footer";
 export default function page() {
   return (
